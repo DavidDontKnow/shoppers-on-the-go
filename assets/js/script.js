55c276d1fc58301d50808e5a7cd2a5d0d87864a4
DavidDontKnow <daviddontknow02@gmail.com>
2023-02-07 14:25:28 -0800

    JS to store user request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,59 @@
-console.log("JavaScript is ready to run");
+// Waits to load DOM
+$(function () {
+
+    let input = $("input")
+
+    $("#search-btn").click(function (e) {
+        e.preventDefault();
+        let searchRequest = input.val()
+        console.log(searchRequest)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
