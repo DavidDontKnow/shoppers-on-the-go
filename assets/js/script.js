7676ef99bad56ef6156077ceaed4805884fd78b6
Ana Campos <arcampos171@gmail.com>
2023-02-07 19:32:24 -0800

    created display function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 console.log("JavaScript is ready to run");
+
+function displayResponse(response) {
+    const responseContainer = document.querySelector('#response-container');
+    responseContainer.textContent = response;
+  }
+  
