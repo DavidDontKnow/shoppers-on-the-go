7ae5357dd7ad6a123cc446e5a2acc96b1af29f2d
DavidDontKnow <DavidDontKnow02@gmail.com>
2023-02-13 22:57:31 -0800

    stopped places api and added modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,47 @@
-function initMap() {
-    // get user location 
-    document.getElementById("submit").addEventListener("click", function () {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var lat = position.coords.latitude;
-            var lng = position.coords.longitude;
-            // init the map service ans store in a var 
-            var service = new google.maps.places.PlacesService(document.createElement('div'));
-            // call the api using service var and nearbySearch()
-            service.nearbySearch({
-                // parameters
-                location: { lat, lng: lng },
-                type: 'grocery_or_supermarket',
-                rankBy: google.maps.places.RankBy.DISTANCE,
-                openNow: true,
-            }, function (results, status) {
-                // if good load then store into an array 
-                if (status === google.maps.places.PlacesServiceStatus.OK) {
-                    var groceryStores = [];
-                    for (var i = 0; i < results.length; i++) {
-                        groceryStores.push(results[i]);
-                    }
-                    console.log(groceryStores)
-                    // pass to function to append to html 
-                    displayGroceryStores(groceryStores);
-                }
-            });
-        });
-    });
-}
+// function initMap() {
+//   // get user location 
+//   document.getElementById("submit").addEventListener("click", function () {
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//       var lat = position.coords.latitude;
+//       var lng = position.coords.longitude;
+//       // init the map service ans store in a var 
+//       var service = new google.maps.places.PlacesService(document.createElement('div'));
+//       // call the api using service var and nearbySearch()
+//       service.nearbySearch({
+//         // parameters
+//         location: { lat, lng: lng },
+//         type: 'grocery_or_supermarket',
+//         rankBy: google.maps.places.RankBy.DISTANCE,
+//         openNow: true,
+//       }, function (results, status) {
+//         // if good load then store into an array 
+//         if (status === google.maps.places.PlacesServiceStatus.OK) {
+//           var groceryStores = [];
+//           for (var i = 0; i < results.length; i++) {
+//             groceryStores.push(results[i]);
+//           }
+//           console.log(groceryStores)
+//           // pass to function to append to html 
+//           displayGroceryStores(groceryStores);
+//         }
+//       });
+//     });
+//   });
+// }
 
-// loop through stores array 
-function displayGroceryStores(stores) {
-    var list = document.getElementById("store-list");
-    list.innerHTML = "";
-    for (var i = 0; i < stores.length; i++) {
-        var store = stores[i];
-        var li = document.createElement("li");
-        li.innerHTML = store.name + " - " + store.vicinity;
-        li.setAttribute("class", "has-text-centered py-2")
-        list.appendChild(li);
-    }
-}
+// // loop through stores array 
+// function displayGroceryStores(stores) {
+//   var list = document.getElementById("store-list");
+//   list.innerHTML = "";
+//   for (var i = 0; i < stores.length; i++) {
+//     var store = stores[i];
+//     var li = document.createElement("li");
+//     li.innerHTML = store.name + " - " + store.vicinity;
+//     li.setAttribute("class", "has-text-centered py-2")
+//     list.appendChild(li);
+//   }
+// }
 
-// Waits to load DOM
 $(function () {
   //get recipe info
   //NOTE: Must first request temporary access from the demo server-- see console to gain access
@@ -77,6 +76,8 @@ $(function () {
         });
       } else {
         console.error("No search term entered");
+        var noInput = $("#no-input-modal")
+        noInput.attr("class", "is-active")
       }
     });
   });
@@ -92,19 +93,41 @@ $(function () {
     renderList(recipe.ingredientLines);
   }
 
-  //var ingre = ["onions", "pickles", "tomato juice"];
+  //var ingredients = ["onions", "pickles", "tomato juice"];
 
   function renderList(ingredientsList) {
     var food = document.getElementById("food");
     food.innerHTML = '';
 
     for (let i = 0; i < ingredientsList.length; i++) {
-        var li = document.createElement("li");
-        li.textContent = ingredientsList[i];
-        li.classList.add('colum');
-        food.append(li);
+      var li = document.createElement("li");
+      li.textContent = ingredientsList[i];
+      li.classList.add('colum');
+      food.append(li);
     }
   }
 
-  //renderList(ingre);
+  //renderList(ingredients);
+
+
+  $(".modal-close").click(function (e) {
+    console.log(e.target)
+    var modal = $(e.target).parent();
+    modal.removeAttr("class")
+    modal.attr("class", "modal")
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
