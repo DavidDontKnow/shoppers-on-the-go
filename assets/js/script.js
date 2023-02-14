221fa9769ae749d01a88c07bdd1670d290b59504
myleen12 <myleen.santana12@icloud.com>
2023-02-13 21:19:15 -0800

    rendering ingredient list from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,136 +1,66 @@
-
 // Waits to load DOM
 $(function () {
+  //get recipe info
+  //NOTE: Must first request temporary access from the demo server-- see console to gain access
+  const appId = "ab033eef";
+  const appKey = "0949c8863c9896ad2df59beb0142e2bc";
 
+  $(document).ready(function () {
+    $("#submit").click(function () {
+      var searchTerm = $("#search-input").val();
+      console.log(searchTerm);
+      if (searchTerm) {
+        var proxy = "https://cors-anywhere.herokuapp.com/";
+        var apiUrl =
+          "https://api.edamam.com/search?q=" +
+          searchTerm +
+          "&app_id=" +
+          appId +
+          "&app_key=" +
+          appKey;
 
-    //get recipe info
-    //NOTE: Must first request temporary access from the demo server-- see console to gain access
-    const appId = "ab033eef";
-    const appKey = "0949c8863c9896ad2df59beb0142e2bc";
-
-    $(document).ready(function () {
-        $("#submit").click(function () {
-            var searchTerm = $("#search-input").val();
-            console.log(searchTerm)
-            if (searchTerm) {
-                var proxy = "https://cors-anywhere.herokuapp.com/";
-                var apiUrl = "https://api.edamam.com/search?q=" + searchTerm + "&app_id=" + appId + "&app_key=" + appKey;
-
-                $.ajax({
-                    url: proxy + apiUrl,
-                    method: "GET",
-                    success: function (data) {
-                        console.log(data);
-                        // process the data to display the recipes information
-                        displayInfo(data)
-                    },
-                    error: function (error) {
-                        console.log(error);
-                    }
-                });
-            } else {
-                console.error("No search term entered");
-            }
+        $.ajax({
+          url: proxy + apiUrl,
+          method: "GET",
+          success: function (data) {
+            console.log(data);
+            // process the data to display the recipes information
+            displayInfo(data);
+          },
+          error: function (error) {
+            console.log(error);
+          },
         });
+      } else {
+        console.error("No search term entered");
+      }
     });
+  });
 
+  function displayInfo(data) {
+    const recipeImg = document.getElementById("recipe-img");
+    const searchButton = document.getElementById("search-button");
 
-    function displayInfo(data) {
-        const recipeImg = document.getElementById('recipe-img');
-        const searchButton = document.getElementById('search-button');
+    const recipe = data.hits[0].recipe;
+    recipeImg.src = recipe.image;
+    recipeImg.alt = recipe.label;
+    searchButton.innerHTML = `<a href="${recipe.url}" target="_blank">View Recipe</a>`;
+    renderList(recipe.ingredientLines);
+  }
 
-        const recipe = data.hits[0].recipe;
-        recipeImg.src = recipe.image;
-        recipeImg.alt = recipe.label;
-        searchButton.innerHTML = `<a href="${recipe.url}" target="_blank">View Recipe</a>`;
+  //var ingre = ["onions", "pickles", "tomato juice"];
+
+  function renderList(ingredientsList) {
+    var food = document.getElementById("food");
+    food.innerHTML = '';
+
+    for (let i = 0; i < ingredientsList.length; i++) {
+        var li = document.createElement("li");
+        li.textContent = ingredientsList[i];
+        li.classList.add('colum');
+        food.append(li);
     }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
-
+  //renderList(ingre);
+});
