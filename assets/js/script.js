25820cc47e0811bad5cbbf857e8f773b9b7821f9
DavidDontKnow <DavidDontKnow02@gmail.com>
2023-02-14 00:46:32 -0800

    uncomment places

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,49 @@
-// function initMap() {
-//   // get user location 
-//   document.getElementById("submit").addEventListener("click", function () {
-//     navigator.geolocation.getCurrentPosition(function (position) {
-//       var lat = position.coords.latitude;
-//       var lng = position.coords.longitude;
-//       // init the map service ans store in a var 
-//       var service = new google.maps.places.PlacesService(document.createElement('div'));
-//       // call the api using service var and nearbySearch()
-//       service.nearbySearch({
-//         // parameters
-//         location: { lat, lng: lng },
-//         type: 'grocery_or_supermarket',
-//         rankBy: google.maps.places.RankBy.DISTANCE,
-//         openNow: true,
-//       }, function (results, status) {
-//         // if good load then store into an array 
-//         if (status === google.maps.places.PlacesServiceStatus.OK) {
-//           var groceryStores = [];
-//           for (var i = 0; i < results.length; i++) {
-//             groceryStores.push(results[i]);
-//           }
-//           console.log(groceryStores)
-//           // pass to function to append to html 
-//           displayGroceryStores(groceryStores);
-//         }
-//       });
-//     });
-//   });
-// }
+function initMap() {
+  // get user location 
+  document.getElementById("submit").addEventListener("click", function () {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // init the map service ans store in a var 
+      var service = new google.maps.places.PlacesService(document.createElement('div'));
+      // call the api using service var and nearbySearch()
+      service.nearbySearch({
+        // parameters
+        location: { lat, lng: lng },
+        type: 'grocery_or_supermarket',
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        openNow: true,
+      }, function (results, status) {
+        // if good load then store into an array 
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          var groceryStores = [];
+          for (var i = 0; i < results.length; i++) {
+            groceryStores.push(results[i]);
+          }
+          console.log(groceryStores)
+          // pass to function to append to html 
+          displayGroceryStores(groceryStores);
+        }
+      });
+    });
+  });
+}
 
-// // loop through stores array 
-// function displayGroceryStores(stores) {
-//   var list = document.getElementById("store-list");
-//   list.innerHTML = "";
-//   for (var i = 0; i < stores.length; i++) {
-//     var store = stores[i];
-//     var li = document.createElement("li");
-//     li.innerHTML = store.name + " - " + store.vicinity;
-//     li.setAttribute("class", "has-text-centered py-2")
-//     list.appendChild(li);
-//   }
-// }
-
+// loop through stores array 
+function displayGroceryStores(stores) {
+  var list = document.getElementById("store-list");
+  list.innerHTML = "";
+  for (var i = 0; i < stores.length; i++) {
+    var store = stores[i];
+    var li = document.createElement("li");
+    li.innerHTML = store.name + " - " + store.vicinity;
+    li.setAttribute("class", "has-text-centered py-2")
+    list.appendChild(li);
+  }
+}
+// waits for dom to load
 $(function () {
-
+  // loads previous recipe ingredients list
   showPrevious()
 
   //get recipe info
@@ -72,6 +72,7 @@ $(function () {
             console.log(data);
             // process the data to display the recipes information
             displayInfo(data);
+            // saves data to local storage 
             window.localStorage.setItem("lastSearch", JSON.stringify(data))
           },
           error: function (error) {
@@ -87,21 +88,25 @@ $(function () {
     });
   });
 
+  // displays all info from recipes api to html
   function displayInfo(data) {
     const recipeImg = document.getElementById("recipe-img");
     const searchButton = document.getElementById("search-button");
 
     const recipe = data.hits[0].recipe;
+    // sets img src
     recipeImg.src = recipe.image;
+    // sets alt text
     recipeImg.alt = recipe.label;
+    // sets link to full article in new tab
     searchButton.innerHTML = `<a href="${recipe.url}" target="_blank">View Recipe</a>`;
+    // runs function to loop through ingredients 
     renderList(recipe.ingredientLines);
   }
 
-  //var ingredients = ["onions", "pickles", "tomato juice"];
-
   function renderList(ingredientsList) {
     var food = document.getElementById("food");
+    // removes so there are no duplicates 
     food.innerHTML = '';
 
     for (let i = 0; i < ingredientsList.length; i++) {
@@ -112,15 +117,11 @@ $(function () {
     }
   }
 
-  //renderList(ingredients);
-
-
   $(".modal-close").click(function (e) {
     var modal = $(e.target).parent();
     modal.removeAttr("class")
     modal.attr("class", "modal")
   })
-
 
   function showPrevious() {
     if (window.localStorage.getItem("lastSearch") === null) {
@@ -130,15 +131,6 @@ $(function () {
       displayInfo(data)
     }
   }
-
-
-
-
-
-
-
-
-
 
 
 });
